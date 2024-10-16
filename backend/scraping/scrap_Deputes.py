73b277f4eb4586f9558bc33eed2fc9d5583127ae
marion <umoya@Pelagornis>
2024-10-16 10:52:21 +0200

    Scrape current parties and start to scrape parties members and presidents

--- backend/scraping/scrap_Deputes.py
+++ backend/scraping/scrap_Deputes.py
@@ -2,7 +2,14 @@
 
 import requests
 from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 #from unidecode import unidecode
+
+driver = webdriver.Chrome()
 
 # Set up logging configuration
 # Function to scrape the href links of all sites from the specified URL
@@ -22,30 +29,41 @@ def scrape_political_groups(url):
             for a in div.find_all("a"):
                 href = a.get("href")
                 political_groups_links.append(href)
-        #print(political_groups_links)
         return political_groups_links
     
 def scrape_party(political_groups: list) -> list:
     parties = []
+    parti_president = []
+    parties_members = []
     for party in political_groups:
-        response = requests.get(party)
+        # response = requests.get(party)
+        response = driver.get("party")
         if response.status_code == 200:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "ajax-wrapper"))
+            )
             soup = BeautifulSoup(response.content, "html.parser")
+
+            # scrape party name
             party_name = soup.h1.string
             parties.append(party_name)
+
+            # scrape parties members and president
+            for a in soup.find_all("a", class_="instance-composition-nom"):
+                parties_members.append(a.string)
+            
+            test = soup.h3
+            
+
     print(parties)
+    print(parties_members)
+    print(test)
+    return parties
 
-
-
-
-  
-
-        
 
 
 political_groups_links = scrape_political_groups("https://www.assemblee-nationale.fr/dyn/les-groupes-politiques")
 scrape_party(political_groups_links)
-
 # Function to construct URLs link for each site    
 
 # Function to scrape each sites and retrieve its routes types
