068c16d2f67f6ac35fda046853776a36c7b6aeb1
Marion <marion.ochem@gmail.com>
2024-11-29 12:30:01 +0100

    Add file update todo

---
+++ scraping/update/pol_groups_changes.py
@@ -0,0 +1,10 @@
+"Changes to the composition of the groups"
+
+"""
+1. Scrape : https://www2.assemblee-nationale.fr/17/les-groupes-politiques/modifications-a-la-composition-des-groupes
+2. Check for new date entry (create a file to store last modification date)
+3. If new entry : 
+    get supressed name > supress them from db
+    get added name > scrape them and add them to db
+
+"""
