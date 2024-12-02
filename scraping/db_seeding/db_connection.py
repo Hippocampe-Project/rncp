358cb49508668227228abe1d2d1d4fb97836d271
Marion <marion.ochem@gmail.com>
2024-12-02 22:24:14 +0100

    Remove unecessary env variable intiliatisation in db_connection.py

--- scraping/db_seeding/db_connection.py
+++ scraping/db_seeding/db_connection.py
@@ -4,11 +4,6 @@ import logging
 import dotenv
 
 dotenv.load_dotenv()
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
 
 
 class HandleDBConnection:
