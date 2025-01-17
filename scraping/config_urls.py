2e72477d567d79d3fad9cd81fd7f6306e01c8690
Marion <marion.ochem@gmail.com>
2025-01-17 15:57:42 +0100

    Modify main.py logic and add all scraping logic for votes

--- scraping/config_urls.py
+++ scraping/config_urls.py
@@ -1,6 +1,10 @@
 """ File to store all root urls """
 
 BASE_URL = "https://www2.assemblee-nationale.fr"
-POLITICAL_GROUPS_URLS = "https://www.assemblee-nationale.fr/dyn/les-groupes-politiques"
-COMMISSIONS_URL = "https://www.assemblee-nationale.fr/dyn/commissions-et-autres-organes"
-DEPARTEMENTS_URLS = "https://www2.assemblee-nationale.fr/deputes/liste/departements"
+POLITICAL_GROUPS_URLS = f"{BASE_URL}/dyn/les-groupes-politiques"
+COMMISSIONS_URL = f"{BASE_URL}/dyn/commissions-et-autres-organes"
+DEPARTEMENTS_URLS = f"{BASE_URL}/deputes/liste/departements"
+RECORDED_VOTE_URL = f"{BASE_URL}/dyn/17/scrutins?limit=100"  # 100 results / page
+GOVERNMENT_BILLS = f"{BASE_URL}/documents/liste/(type)/projets-loi"
+PARLEMENT_BILLS = f"{BASE_URL}/documents/liste/(type)/propositions-loi"
+ENACTED_BILLS = f"{BASE_URL}/documents/liste/(type)/ta"  # à vérifier
