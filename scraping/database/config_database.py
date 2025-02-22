b4cd039c240cf530d0dbfe1d214cd646488dff26
MarionOchem <marion.ochem@gmail.com>
2025-02-22 21:23:51 +0100

    Add new table name in config_database for deputes update purpose

--- scraping/database/config_database.py
+++ scraping/database/config_database.py
@@ -1,7 +1,8 @@
-""" File containing all db tables names """
+"""File containing all db tables names"""
 
 DEPARTEMENTS_TABLE = "departements"
 COMMISSIONS_TABLE = "commissions_p"
 PARTIES_TABLE = "partis"
 REPRESENTATIVES_TABLE = "deputes"
 VOTES_TABLE = "votes"
+TEMP_REPRESENTATIVES_TABLE = "temp_deputes"
