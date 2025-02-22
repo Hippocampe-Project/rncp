386d22be30fd1b7e56eb26d4c63db44a1d3f70b3
MarionOchem <marion.ochem@gmail.com>
2025-02-22 21:32:51 +0100

    Add the docker mount point of persistent scraping+updating data and add filepath of several json files

--- scraping/config_urls.py
+++ scraping/config_urls.py
@@ -1,4 +1,4 @@
-""" File to store all root urls """
+"""File to store all root urls"""
 
 BASE_URL = "https://www2.assemblee-nationale.fr"
 POLITICAL_GROUPS_URLS = f"{BASE_URL}/dyn/les-groupes-politiques"
@@ -8,6 +8,11 @@ RECORDED_VOTE_URL = f"{BASE_URL}/dyn/17/scrutins?limit=100"  # 100 results / pag
 GOVERNMENT_BILLS = f"{BASE_URL}/documents/liste/(type)/projets-loi"
 PARLEMENT_BILLS = f"{BASE_URL}/documents/liste/(type)/propositions-loi"
 ENACTED_BILLS = f"{BASE_URL}/documents/liste/(type)/ta"  # à vérifier
+POL_GROUP_CHANGES = (
+    f"{BASE_URL}/17/les-groupes-politiques/modifications-a-la-composition-des-groupes"
+)
 
-LAST_VOTE_FILE = "/scraping/persistent_data/last_scrapped_vote.json" # this is the container mount point to "/home/marion/dev/rncp_project/last_scrapped_vote.json"
-
+# /scraping/persistent_data == container mount point to "/home/marion/dev/rncp_project
+LAST_SCRAPED_VOTE_FILE = "/scraping/persistent_data/last_scraped_vote.json"
+LAST_SCRAPING_INFOS = "/scraping/persistent_data/last_scraping_infos.json"
+LAST_SCRAPED_DEPUTES_FILE = "/scraping/persistent_data/last_scraped_deputes.json"
