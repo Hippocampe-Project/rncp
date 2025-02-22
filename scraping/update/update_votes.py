c7b3db090691cf03969abbf22b9402804abf860b
MarionOchem <marion.ochem@gmail.com>
2025-02-22 21:17:06 +0100

    Delete update_vote.py file

--- scraping/update/update_votes.py
+++
@@ -1,7 +0,0 @@
-import requests
-import logging
-from bs4 import BeautifulSoup
-from config_urls import BASE_URL
-from scrape.scrape_models import Departement, Commissions
-
-from scrape.scrape_votes import scrape_vote_page
