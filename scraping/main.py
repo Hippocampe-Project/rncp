b16c0a74d68c21e2c29bb1ca7c1cd835686c80f5
MarionOchem <marion.ochem@gmail.com>
2025-02-22 21:36:16 +0100

    Modify main execution logic to inhence script execution autonomy and modify the last_scraping json file for better informations

--- scraping/main.py
+++ scraping/main.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime, time
 import time
 import logging
+import sys
 
 logging.basicConfig(
     level=logging.INFO,
@@ -12,32 +14,41 @@ from config_urls import (
     COMMISSIONS_URL,
     DEPARTEMENTS_URLS,
     RECORDED_VOTE_URL,
-    LAST_VOTE_FILE,
+    LAST_SCRAPED_VOTE_FILE,
+    LAST_SCRAPING_INFOS,
+    LAST_SCRAPED_DEPUTES_FILE,
 )
 
 from chrome_driver_handler import ChromeDriverHandler
 from scrape.scrape_political_parties_urls import scrape_political_parties_urls
-from scrape.scrape_representatives import scrape_every_representative
+from scrape.scrape_representatives import scrape_each_representative
 from scrape.scrape_permanent_infos import scrape_departements, scrape_commissions
 from scrape.scrape_each_political_group_page import scrape_each_political_group_page
 from scrape.scrape_votes import scrape_all_votes_urls, scrape_each_vote
 from database.db_insertions import (
-    first_scraping_database_insertion,
-    second_scraping_database_insertion,
+    pol_groups_and_deputes_database_insertion,
+    votes_database_insertion,
+    update_deputes_database_table,
+    permanent_infos_database_insertion,
 )
-from scrape_utils import sort_votes_by_vote_number, save_json
+from scrape_utils import sort_votes_by_vote_number, save_json, load_json
+from update.should_update import should_update_deputes
+from database.db_operations import HandleDatabase
 
 # Access the environment variables
 chrome_bin = os.getenv("CHROME_BIN")
 chrome_driver = os.getenv("CHROME_DRIVER")
 
-# departements, permanent commissions, political parties, representatives
-first_scraping = False
-# votes
-second_scraping = True
+format_date = datetime.now().strftime("%d-%m-%Y")
+today_date = datetime.strptime(format_date, "%d-%m-%Y")
 
-database_insertion = True
-updating_votes = True
+# Scraping configuration
+permanent_infos = False
+scrape_pol_groups_and_deputes = False
+scrape_votes = False
+database_insertion = False
+updating_votes = False
+updating_deputes = False
 
 
 def main():
@@ -50,7 +61,11 @@ def main():
 
     driver_handler = ChromeDriverHandler(chrome_bin, chrome_driver)
 
-    if first_scraping:
+    # fmt: off
+    scrape_pol_groups_and_deputes = database_insertion = updating_deputes = should_update_deputes(LAST_SCRAPED_DEPUTES_FILE)
+    # fmt: on
+
+    if permanent_infos:
 
         departements_table = scrape_departements(DEPARTEMENTS_URLS)
         logging.info(departements_table)
@@ -58,16 +73,18 @@ def main():
         commissions_table = scrape_commissions(COMMISSIONS_URL)
         logging.info(commissions_table)
 
+    if scrape_pol_groups_and_deputes:
+
         political_groups_links = scrape_political_parties_urls(POLITICAL_GROUPS_URLS)
         parties_table, all_representatives_urls = scrape_each_political_group_page(
             driver_handler, political_groups_links
         )
         logging.info(parties_table)
 
-        representatives_table = scrape_every_representative(all_representatives_urls)
+        representatives_table = scrape_each_representative(all_representatives_urls)
         logging.info(representatives_table)
 
-    if second_scraping:
+    if scrape_votes:
         if updating_votes:
             all_votes_pages_urls = scrape_all_votes_urls(
                 driver_handler, RECORDED_VOTE_URL, updating=True
@@ -76,14 +93,13 @@ def main():
             all_votes_pages_urls = scrape_all_votes_urls(
                 driver_handler, RECORDED_VOTE_URL
             )
+        if (
+            len(all_votes_pages_urls) == 0
+        ):  # sometimes scraping fail to retrieve any urls fo r unknown reason for some times
+            sys.exit(1)
         all_votes_data = scrape_each_vote(all_votes_pages_urls)
         all_votes_infos_sorted = sort_votes_by_vote_number(all_votes_data)
         logging.info(f"all votes length : {len(all_votes_infos_sorted)}")
-        save_json(
-            {"last_scrapped_vote": all_votes_infos_sorted[0]["numero_vote"]},
-            LAST_VOTE_FILE,
-        )
-
 
     end_scraping = time.time()
     logging.info(f" ~ Scraping execution time : {start_scraping - end_scraping} ~ ")
@@ -92,18 +108,75 @@ def main():
         logging.info(" - Starting database insertion - ")
         start_db_insertion = time.time()
 
-        if first_scraping:
-            first_scraping_database_insertion(
-                departements_table, commissions_table, parties_table, representatives_table
+        db = HandleDatabase()
+        vote_update_success = None
+        deputes_update_success = None
+
+        if permanent_infos:
+            permanent_infos_database_insertion(
+                db, departements_table, commissions_table
             )
 
-        if second_scraping:
-            second_scraping_database_insertion(all_votes_infos_sorted)
+        if updating_deputes:
+            update_deputes_database_table(db, representatives_table)
+            if db.query_success == True:
+                save_json(
+                    {"last_scraped_deputes": format_date}, LAST_SCRAPED_DEPUTES_FILE
+                )
+                deputes_update_success = True
+        elif scrape_pol_groups_and_deputes:
+            pol_groups_and_deputes_database_insertion(
+                db,
+                parties_table,
+                representatives_table,
+            )
+            if db.query_success == True:
+                save_json(
+                    {"last_scraped_deputes": format_date}, LAST_SCRAPED_DEPUTES_FILE
+                )
+                deputes_update_success = True
+
+        if scrape_votes:
+            votes_database_insertion(db, all_votes_infos_sorted)
+            if db.query_success == True:
+                save_json(
+                    {"last_scraped_vote": all_votes_infos_sorted[0]["numero_vote"]},
+                    LAST_SCRAPED_VOTE_FILE,
+                )
+                vote_update_success = True
 
         end_db_insertion = time.time()
         logging.info(
             f" ~ Database insertion execution time : {start_db_insertion - end_db_insertion} ~ "
         )
+
+    logging.info("Saving this scraping execution infos")
+    save_json(
+        {
+            "last_scraping": {
+                "date": format_date,
+                "pol_groups_and_deputes": {
+                    "scraping_attempt": scrape_pol_groups_and_deputes,
+                    "scraping_success": None,
+                    "update_attempt": (
+                        True
+                        if database_insertion and scrape_pol_groups_and_deputes
+                        else False
+                    ),
+                    "update_success": deputes_update_success,
+                },
+                "votes": {
+                    "scraping_attempt": scrape_votes,
+                    "scraping_success": None,
+                    "update_attempt": (
+                        True if database_insertion and updating_votes else False
+                    ),
+                    "update_success": vote_update_success,
+                },
+            }
+        },
+        LAST_SCRAPING_INFOS,
+    )
 
     end_time = time.time()
     logging.info(f" ~ Execution time : {end_time - start_time} ~ ")
