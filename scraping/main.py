0fb5b924470e28834c812d083b0ef1854088aa75
MarionOchem <marion.ochem@gmail.com>
2025-04-25 17:43:33 +0200

    log file format change

--- scraping/main.py
+++ scraping/main.py
@@ -13,8 +13,7 @@ LOGS_PATH = os.getenv("LOGS_PATH")
 
 format_date = datetime.now().strftime("%d-%m-%Y")
 today_date = datetime.strptime(format_date, "%d-%m-%Y")
-timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 log_filename = os.path.join(LOGS_PATH, f"log_{timestamp}.log")
 logging.basicConfig(
