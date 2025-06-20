7494b3360ecb5406031b90f0ba659e67eeb65e9d
MarionOchem <marion.ochem@gmail.com>
2025-06-20 15:23:58 +0200

    Remove local getenv import and uses new globals.config_variables instead, improve email alert format and modify custom_exit() logic

--- scraping/error_handler.py
+++ scraping/error_handler.py
@@ -3,23 +3,22 @@ import os
 import logging
 import smtplib
 import traceback
-from dotenv import load_dotenv
 
-load_dotenv()
-
-
-# Email Configurations
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 465
-EMAIL_SENDER = os.getenv("EMAIL")
-EMAIL_RECEIVER = os.getenv("EMAIL")
-EMAIL_PASSWORD = os.getenv("APP_PASSWORD")
+from globals.config_variables import (
+    SMTP_PORT,
+    SMTP_SERVER,
+    EMAIL_PASSWORD,
+    EMAIL_RECEIVER,
+    EMAIL_SENDER,
+)
 
 
-def send_error_email(error_message):
+def send_error_email(content: str):
     """Sends an email with the error details."""
+
+    logging.warning("Sending alert email...")
     subject = "🚨 RNCP Scraping Error Alert"
-    body = f"An error occurred in the script:\n\n{error_message}"
+    body = f"An error occurred during scraping execution :\n\n{content}"
 
     email_message = f"Subject: {subject}\n\n{body}"
 
@@ -28,15 +27,20 @@ def send_error_email(error_message):
             server.starttls()
             server.login(EMAIL_SENDER, EMAIL_PASSWORD)
             server.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, email_message)
+            logging.info("Alert email sent.")
     except Exception as e:
         logging.error(f"Failed to send email: {e}")
 
 
-def custom_exit(status=1):
+def custom_exit(error_msg: str):
     """Handles errors and sends an email before exiting."""
-    error_message = "".join(traceback.format_stack())
-    send_error_email(error_message)
-    sys.__exit(status)  # Call the original sys.exit
-
-
-sys.exit = custom_exit  # Override sys.exit globally
+    logging.warning("Major failure occurred")
+    stack = "".join(traceback.format_stack())
+    alert_content = (
+        f"🚨 RNCP Scraper Failure\n\n"
+        f"📄 Error message:\n{error_msg}\n\n"
+        f"🧵 Stack trace:\n{stack}"
+    )
+    send_error_email(alert_content)
+    logging.warning("EXITING NOW")
+    sys.exit(1)
