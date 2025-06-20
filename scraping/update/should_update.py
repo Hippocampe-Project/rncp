291ad7b26d29c1e09f5400da00a06e97a43c618a
MarionOchem <marion.ochem@gmail.com>
2025-06-20 12:53:29 +0200

    Add logging info for tracking / debuging inside should_update.py

--- scraping/update/should_update.py
+++ scraping/update/should_update.py
@@ -6,11 +6,18 @@ from update.pol_groups_changes import scrape_last_groupe_composition_change
 
 
 def should_update_deputes(filepath) -> bool:
+    logging.info("Searching for changes in political group composition ...")
     last_deputes_scraping_infos = load_json(filepath)
     last_deputes_scraping_date: str = last_deputes_scraping_infos.get(
         "last_scraped_deputes"
     )
+    logging.info(
+        f"Last time political group composition was udpated : {last_deputes_scraping_date}"
+    )
     last_group_modification_date: datetime = scrape_last_groupe_composition_change()
+    logging.info(
+        f"Last political group composition modification : {last_group_modification_date}"
+    )
     if last_group_modification_date > datetime.strptime(
         last_deputes_scraping_date, "%d-%m-%Y"
     ):
