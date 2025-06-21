36f17506ddd0c8cef122f62053c489fc7ad9357b
MarionOchem <marion.ochem@gmail.com>
2025-06-21 17:50:42 +0200

    Fix email using SMTP over SSL and email UTF-8 content

--- scraping/error_handler.py
+++ scraping/error_handler.py
@@ -3,6 +3,8 @@ import os
 import logging
 import smtplib
 import traceback
+from email.message import EmailMessage
+
 
 from globals.config_variables import (
     SMTP_PORT,
@@ -21,15 +23,19 @@ def send_error_email(content: str):
     body = f"An error occurred during scraping execution :\n\n{content}"
 
     email_message = f"Subject: {subject}\n\n{body}"
+    email_message = EmailMessage()
+    email_message["Subject"] = subject
+    email_message["From"] = EMAIL_SENDER
+    email_message["To"] = EMAIL_RECEIVER
+    email_message.set_content(body)
 
     try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
+        with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
             server.login(EMAIL_SENDER, EMAIL_PASSWORD)
-            server.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, email_message)
+            server.send_message(email_message)
             logging.info("Alert email sent.")
     except Exception as e:
-        logging.error(f"Failed to send email: {e}")
+        logging.error(f"Failed to send email : {e}")
 
 
 def custom_exit(error_msg: str):
