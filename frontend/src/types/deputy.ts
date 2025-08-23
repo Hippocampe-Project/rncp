9b6c1c52d133fe56e54bdeefee689c288269117f
MarionOchem <marion.ochem@gmail.com>
2025-08-23 09:16:56 +0200

    Add interface for Depute and Vote in types/ dir

---
+++ frontend/src/types/deputy.ts
@@ -0,0 +1,15 @@
+export interface Deputy {
+  id: number;
+  nom: string;
+  date_naissance: string;
+  sexe: string;
+  departement: string;
+  circonscription: string;
+  commission_permanente: string;
+  profession: string;
+  suppleant?: string;
+  parti: string;
+  photo?: string;
+  activite: boolean;
+  activite_timestamp?: string; // GraphQL will serialize Date as string
+}
